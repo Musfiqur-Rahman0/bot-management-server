119842f0b5a054b60b95e7e4db594b17163d75ac
Musfiqur-Rahman0 <musfiqurrhaman6@gmail.com>
2025-05-13 16:55:59 +0600

    practise curd with mongodb node and express js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-
+const { MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
+const uri = "mongodb+srv://musfiqurrhaman6:z7S7XCzvQU05RBqe@cluster0.nliquld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const app  = express();
 const port = 3000;
 
@@ -8,41 +9,63 @@ const port = 3000;
 app.use(cors());
 app.use(express.json())
 
+const client = new MongoClient(uri, {
+    serverApi : {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
 
-const bots = [
-    {
-        id: 1,
-        name: "sakib",
-        email: "botsakib@gmail.com"
-    },
-    {
-        id: 2,
-        name: "makib",
-        email: "botmakib@gmail.com"
-    },
-    {
-        id: 3,
-        name: "takib",
-        email: "bottakib@gmail.com"
-    },
-]
 
 app.get("/", (req, res)=> {
     res.send("Hellow world!")
 });
 
-app.get("/bots", (req, res)=> {
-    res.send(bots);
-});
 
-app.post("/bots", (req, res)=> {
-    // console.log(req.body);
-    const newBot = req.body ;
-    console.log(newBot);
-    
-    bots.push(newBot);
-    console.log(bots)
-})
+
+
+const run = async() => {
+    try {
+
+        const botsCollection = client.db("botsdb").collection("bots");
+        app.get("/bots", async(req, res)=> {
+            const cursor = botsCollection.find();
+            const result =await cursor.toArray();
+            res.send(result);
+        })
+
+
+        app.post("/bots", async(req, res)=> {
+            console.log("data from the client", req.body);
+            const newBot = req.body;
+            const result= await botsCollection.insertOne(newBot);
+            res.send(result);
+        });
+
+        app.delete("/bots/:id", async(req, res)=> {
+            console.log("id to be deleted", req.params.id);
+            const id = req.params.id;
+            const query= { _id : new ObjectId(id)};
+            const result = await botsCollection.deleteOne(query);
+            res.send(result)
+        })
+
+        
+
+        await client.connect(); 
+        await client.db("admin").command({ping  : 1});
+           console.log("Pinged your deployment. You successfully connected to MongoDB!");              
+    }finally{
+
+    }
+}
+
+
+run().catch(console.dir)
+
+
+
 
 
 
